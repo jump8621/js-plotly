5500095d162a59b051ccfe9cdd1aa990b1e1ce73
Juliann Pezzullo <jump8621@gmail.com>
2020-12-15 22:42:54 -0500

    add data

--- static/js/app.js
+++ static/js/app.js
@@ -164,17 +164,17 @@ function Plotbar(value) {
 function bubbleChart(value) {
   var filteredData = samples.filter(event => parseInt(event.id) === parseInt(value))[0];
   var sample_values = filteredData.sample_values;
-  var otu_ids = filteredData.otu_ids;
+  var otuIDs = filteredData.otu_ids;
   var otu_labels = filteredData.otu_labels;
 
   var trace1 = {
-    x: otu_ids,
+    x: otuIDs,
     y: sample_values,
     mode: 'markers',
     text: otu_labels,
     marker: {
-      color: otu_ids,
-      opacity: otu_ids.map(id => 0.7),
+      color: otuIDs,
+      opacity: otuIDs.map(id => 0.7),
       size: sample_values
     }
   };
@@ -195,12 +195,31 @@ function gaugeChart(value) {
 
   var filteredData = metadata.filter(event => parseInt(event.id) === parseInt(value))[0];
   var wfreq = filteredData.wfreq;
-   
+  
+  // var level = parseFloat(wfreq) * 20;   
+
+  // var degrees = 180 - level,
+  // var radius = 0.5;
+  // var radians = degrees * Math.PI / 180;
+  // var x = radius * Math.cos(radians);
+  // var y = radius * Math.sin(radians);
+  
+  // // Path: may have to change to create a better triangle
+  // var mainPath = 'M -.0 -0.035 L .0 0.035 L ',
+  // var pathX = String(x),
+  // var space = " ",
+  // var pathY = String(y),
+  // var pathEnd = " Z";
+  // var path = mainPath.concat(pathX,space,pathY,pathEnd);
+  
+
+
 
   var trace = [
     {
       domain: { x:[0,1], y: [0,1] },
       value: wfreq,
+      title: {text: "Frequency of Weekly Belly Button Washing"},
       type: "indicator",
       mode: "gauge+number",
       gauge: {
@@ -219,278 +238,23 @@ function gaugeChart(value) {
     }
   ];
 
+
+
+
+  // var layout = {
+  //   shapes: [
+  //     {
+  //       type: "path",
+  //       path: path,
+  //       fillcolor: 850000,
+  //       line: {
+  //         color: 850000
+  //       }
+  //     }
+  //   ]
+  // }
   Plotly.newPlot("gauge", trace) 
 }
-
-// function gaugePointer(value){
-//   var degrees = 180 - value,
-//   radius = .5;
-//   var radians = degrees * Math.PI / 180;
-//   var x = radius * Math.cos(radians);
-//   var y = radius * Math.sin(radians);
-  
-//   // Path: may have to change to create a better triangle
-//   var mainPath = 'M -.0 -0.035 L .0 0.035 L ',
-//   pathX = String(x),
-//   space = " ",
-//   pathY = String(y),
-//   pathEnd = " Z";
-//   var path = mainPath.concat(pathX,space,pathY,pathEnd);
-  
-//   return path;
-//  }
-  
-
-
-
-
-
-
-
-
-//   var data = [
-//     {
-//       type: "indicator",
-//       mode: "gauge+number+delta",
-//       value: 420,
-//       title: { text: "Speed", font: { size: 24 } },
-//       delta: { reference: 400, increasing: { color: "RebeccaPurple" } },
-//       gauge: {
-//         axis: { range: [null, 500], tickwidth: 1, tickcolor: "darkblue" },
-//         bar: { color: "darkblue" },
-//         bgcolor: "white",
-//         borderwidth: 2,
-//         bordercolor: "gray",
-//         steps: [
-//           { range: [0, 250], color: "cyan" },
-//           { range: [250, 400], color: "royalblue" }
-//         ],
-//         threshold: {
-//           line: { color: "red", width: 4 },
-//           thickness: 0.75,
-//           value: 490
-//         }
-//       }
-//     }
-//   ];
-  
-//   var layout = {
-//     width: 500,
-//     height: 400,
-//     margin: { t: 25, r: 25, l: 25, b: 25 },
-//     paper_bgcolor: "lavender",
-//     font: { color: "darkblue", family: "Arial" }
-//   };
-  
-//   Plotly.newPlot('gauge', data, layout);
-
-
-// }
-// init
-    // metadata.forEach(metadata=> {
-    //   var option = selection.append("option");
-    //   option.property("value", metadata);
-    //   option.text(metadata)
-    // })
-
-//     console.log(id, metadata)
-    
-
-//     var selectValues = importedData.names;
-
-//     var selectOpt = d3.select();
-
-//     // selectValues.forEach(value => {
-//     //   selectOpt
-//     //     .append("option")
-//     //     .text(value)
-//     //     .attr("value", function() {
-//     //       return value;
-//     //     });
-//     // })
-  
-
-// //start filling the data inside the select option
-// init();
-
-// //
-
-// d3.selectAll("#selDataset").on("change", plotFunctions);
-
-// function plotFunctions() {
-//   var valueSelect = d3.select("#selDataset").node().value;
-//   demographicdata(valueSelect);
-
-// }
-
-// function demographicdata (valueSelect) {
-// //*****Demographic Data****** */
-//   //var valueSelect = d3.select("#selDataset").node().value;
-//   var selectValues = importedData.names;
-
-//   var selectOpt = d3.select("#selDataset");
-
-
-
-
-
-
-//   //var sortotu_ids = data.sort((a, b) => b.ple_values - a.sample_values);
-//   slicedData = samplesfilter.slice(0, 10);  
-//   var reversedData = slicedData.reverse();
-
-//   //map or filter?
-//   var otu_id = reversedData.map(object => object.out_ids);
-//   var sample_value = reversedData.map(object => object.sample_values);
-//   var labels = reversedData.map(object => object.otu_labels)
-
-//   // data.sort(function(a, b) {
-//   //   return parseFloat(b.samples.id) - parse(a. samples.id) 
-//   // });
-//   // data = data.reverse()
-
-//   var trace1 = {
-//     x: otu_id,
-//     y: sample_value,
-//     text: labels,
-//     type: "bar",
-//     orientation: "h"
-//   };
-  
-//   var chartData = [trace1];
-//   Plotly.newPlot("plot", chartData);
-// });
- 
-// var select = d3.select("#selDataset");
-// select.on("change(this.value)", function () {
-//   var value = d3.event.target.data.id;
-//   //would it be equal to data.samples or do i need to make it equal to a var and then put that after target?
-//   // var samplesfilter = data.filter(data => data.samples === samples)
-//   // slicedData = samplesfilter.slice(0, 10);  
-//   // var reversedData = slicedData.reverse();
-
-
-//   //function newTable () {
-//     d3.event.preventDefault();
-//     var input = d3.select("selDataset");
-//     var person_id = input.property("value");
-//     buildBarGraph(person_id);
-//   //}
-// })
-
-
-//this logic doesn't worki already have the outline built, or maybe it does
-
-//d3.select("selDataset").on("change", newTable);
-
-//change(this.value)
-// d3.selectAll("#selDataset").on("change", function (){
-//   var value = d3.event.target.value;
-//   console.log(value);
-// })
-
-
-// var us = Object.values(data.us);
-// var uk = Object.values(data.uk);
-// var canada = Object.values(data.canada);
-
-// // @ADD YOUR CODE HERE
-
-// var labels = Object.keys(data.us)
-
-// function init () {
-//     var data = [{
-//         values: us,
-//         labels: labels,
-//         type: "pie" 
-//     }];
-
-//     var layout = {
-//         height: 600,
-//         width: 800
-//       };
-
-//     Plotly.newPlot("pie", data, layout);
-// } 
-// d3.selectAll("#selDataset").on("change", getData);
-
-// function getData() {
-//     var dropdownMenu = d3.select("#selDataset");
-//     // Assign the value of the dropdown menu option to a variable
-//     var dataset = dropdownMenu.property("value");
-//     // Initialize an empty array for the country's data
-//     var data = [];
-  
-//     if (dataset == 'us') {
-//         data = us;
-//     }
-//     else if (dataset == 'uk') {
-//         data = uk;
-//     }
-//     else if (dataset == 'canada') {
-//         data = canada;
-//     }
-//     // Call function to update the chart
-//     updatePlotly(data);
-//   }
-  
-//   // Update the restyled plot's values
-//   function updatePlotly(newdata) {
-//     Plotly.restyle("pie", "values", [newdata]);
-//   }
-  
-//   init();
-
-
-
-
-
-// d3.selectAll("#selDataset").on("change", updatePlotly);
-
-// // This function is called when a dropdown menu item is selected
-// function updatePlotly() {
-//   // Use D3 to select the dropdown menu
-//   var dropdownMenu = d3.select("#selDataset");
-//   // Assign the value of the dropdown menu option to a variable
-//   var dataset = dropdownMenu.property("value");
-// }
-
-// var select = d3.select("#select-data");
-
-// select.on('change', function () {
-//     var value = d3.event.target.value;
-//     console.log(value);
-// })
-
-  
-// //  var trace1 = {
-// //     x: data.otu_ids,
-// //     y: data.sample_values.map(val => Math.sqrt(val)),
-// //     type: "bar",
-// //     name:"otu_labels"
-// //   };
-
-// //   var data = [trace1];
-
-// //   Plotly.newPlot("bar", data)
-
-// })
-
-// d3.json("samples.json").then((samples) => {
-//   Object.keys = samples
-// })
-
-
-// //
-// //************so the first function should call on init function I believ******
-// ////****also check about src codes**** */
-
-
-// promise_1().then(function () {
-//   return promise_2();
-// }).then(function () {
-//   return promise_3();
-// }).catch(console.log.bind(console));
 
 
 
